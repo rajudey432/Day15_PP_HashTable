8ec49ed3d45281e4fe8a591923ad24d361b15e1a
raju dey <rajudey432@gmail.com>
2023-05-06 20:35:36 +0530

    Added: Find Frequency of Word

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import static com.bridgelabz.HashTables.WordFrequency.calculateWordFrequency;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("");
         String sentence = "To be or not to be";
         calculateWordFrequency(sentence);
     }
