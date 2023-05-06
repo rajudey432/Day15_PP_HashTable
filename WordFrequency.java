a40930590bf52331f87d2d42a843a594c121732b
raju dey <rajudey432@gmail.com>
2023-05-06 20:40:59 +0530

    Added: Ability to find frequency of words in a large paragraph phrase

--- WordFrequency.java
+++ WordFrequency.java
@@ -2,72 +2,58 @@ package com.bridgelabz.HashTables;
 
 import java.util.*;
 
+import java.util.LinkedList;
+
 public class WordFrequency {
 
-    // MyMapNode to store key-value pair for each word
-    static class MyMapNode {
-        String key;
-        Integer value;
-        MyMapNode next;
+    public static void main(String[] args) {
 
-        public MyMapNode(String key, Integer value) {
-            this.key = key;
-            this.value = value;
-            next = null;
-        }
-    }
+        // The paragraph to analyze
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-    // method to calculate frequency of words using Hash Table implemented with LinkedList
-    public static void calculateWordFrequency(String sentence) {
+        // Convert the paragraph to lowercase and split it into words
+        String[] words = paragraph.toLowerCase().split(" ");
 
-        // convert sentence to lowercase and split into words
-        String[] words = sentence.toLowerCase().split(" ");
+        // Create an array of LinkedLists to store the words and their frequencies
+        LinkedList<MyMapNode>[] wordList = new LinkedList[words.length];
 
-        // create empty hash table with size of array
-        LinkedList<MyMapNode> hashTable[] = new LinkedList[words.length];
+        // Initialize the LinkedLists
         for (int i = 0; i < words.length; i++) {
-            hashTable[i] = new LinkedList<>();
+            wordList[i] = new LinkedList<>();
         }
 
-        // iterate through each word in sentence and update hash table
-        for (String word : words) {
+        // Loop through the words and store their frequencies
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            int index = Math.abs(word.hashCode() % words.length);
 
-            // calculate hash code for current word
-            int hashCode = Math.abs(word.hashCode() % words.length);
-
-            // search for word in hash table
-            LinkedList<MyMapNode> linkedList = hashTable[hashCode];
-            MyMapNode mapNode = search(linkedList, word);
-
-            // if word not found, add it to hash table with count of 1
-            if (mapNode == null) {
-                MyMapNode newNode = new MyMapNode(word, 1);
-                linkedList.add(newNode);
+            // Search for the word in the LinkedList at the corresponding index
+            MyMapNode node = null;
+            LinkedList<MyMapNode> list = wordList[index];
+            for (MyMapNode n : list) {
+                if (n.getKey().equals(word)) {
+                    node = n;
+                    break;
+                }
             }
-            // if word found, increment its count
+
+            // If the word is not found, add it to the LinkedList with frequency 1
+            if (node == null) {
+                node = new MyMapNode(word, 1);
+                list.add(node);
+            }
+            // If the word is found, increment its frequency
             else {
-                mapNode.value += 1;
+                node.setValue(node.getValue() + 1);
             }
         }
 
-        // print frequency of each word
-        for (LinkedList<MyMapNode> linkedList : hashTable) {
-            for (MyMapNode node : linkedList) {
-                System.out.println(node.key + " : " + node.value);
+        // Print the frequencies of the words
+        for (int i = 0; i < words.length; i++) {
+            LinkedList<MyMapNode> list = wordList[i];
+            for (MyMapNode node : list) {
+                System.out.println(node.getKey() + " : " + node.getValue());
             }
         }
     }
-
-    // method to search for a word in a linked list
-    public static MyMapNode search(LinkedList<MyMapNode> linkedList, String word) {
-        for (MyMapNode node : linkedList) {
-            if (node.key.equals(word)) {
-                return node;
-            }
-        }
-        return null;
-    }
-
-    // main method to test the code
-
-    }
+}
